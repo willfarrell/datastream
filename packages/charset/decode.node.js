23f278b50056e9f673239dcaba27d07a794da88d
will Farrell <willfarrell@proton.me>
2024-08-21 09:20:57 -0600

    chore: version bump

--- packages/charset/decode.node.js
+++ packages/charset/decode.node.js
@@ -20,6 +20,42 @@ export const charsetDecodeStream = ({ charset } = {}, streamOptions) => {
   }
   return createTransformStream(transform, flush, streamOptions)
 }
+
+// TODO to peek ahead and determin charset from that
+/*
+export const charsetDetectDecodeStream = (
+  { charset, peekAhead } = { peekAhead: 0 },
+  streamOptions
+) => {
+  charset = getSupportedEncoding(charset)
+
+  let conv,
+    peekAheadChunk = ''
+
+  const transform = (chunk, enqueue) => {
+    if (!conv && peekahead && peekahead < peekAheadChunk.length) {
+      peekAheadChunk += chunk
+      return
+    }
+    if (!conv) {
+      conv = iconv.getDecoder(charset)
+      chunk = peekAheadChunk + chunk
+    }
+
+    const res = conv.write(chunk)
+    if (res?.length) {
+      enqueue(res, 'utf8')
+    }
+  }
+  const flush = (enqueue) => {
+    const res = conv.end()
+    if (res?.length) {
+      enqueue(res, 'utf8')
+    }
+  }
+  return createTransformStream(transform, flush, streamOptions)
+} */
+
 const getSupportedEncoding = (charset) => {
   if (charset === 'ISO-8859-8-I') charset = 'ISO-8859-8'
   if (!iconv.encodingExists(charset)) charset = 'UTF-8'
